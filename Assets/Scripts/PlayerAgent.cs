57047c3c55d21a5554ad8a72a79a8e5e04d8ad6b
Nathan Knebusch <nateknebusch@gmail.com>
2020-12-01 11:45:05 -0800

    Updating ai agent so that it is more complex to do different tasks

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerAgent : MonoBehaviour
 {
+    private enum thingsToDo // all the categories the ai can do
+    {
+        home,
+        sleep,
+        shop,
+        explore
+    };
+
+    private class allLocations // has all the locations stored
+    {
+        public Transform aiHouse;
+        public Transform shops;
+    }
+
+    private GameObject allGoals;
+    private allLocations aiLocations;
+    private thingsToDo whatsCharacterDoing; // currently what the ai is doing
     private NavMeshAgent playerAgent;
-    private List<Transform> goal = new List<Transform>(); // list of all possible goals to pick from
+    private Transform goal; // current destination for character
     private bool needTask = true; // does the character need a new task to do?
+
+    public string home;
 
     void Start()
     {
-        var tempGameObject = GameObject.FindGameObjectsWithTag("goals");
+        allGoals = GameObject.FindGameObjectWithTag("AllGoals");
 
-        for (int i = 0; i < tempGameObject.Length; i++)
-        {
-            goal.Add(tempGameObject[i].transform);
-        }
+        aiLocations.aiHouse = allGoals.transform.Find(this.name + " house");
+
+        aiLocations.shops = allGoals.transform.Find("shops");
 
         if(TryGetComponent(out NavMeshAgent _mesh)) 
         {
@@ -28,12 +47,44 @@ public class PlayerAgent : MonoBehaviour
     }
 
     private void Update()
-    {
+    {        
         if(needTask) // if has no task yet
         {
-            int i = Random.Range(0, goal.Count);
+            switch (whatsCharacterDoing)
+            {
+                case thingsToDo.home:
+                    // if not home
+                    // go home
+                    // if home
+                    // find something to do in the home
+                    break;
 
-            playerAgent.destination = goal[i].position;
+                case thingsToDo.sleep:
+                    // if not home
+                    // go home
+                    // if home
+                    // sleep
+                    break;
+
+                case thingsToDo.shop:
+                    // choose a shop to go to
+                    // if not at the shop
+                    // go to the shop
+                    // if at the shop
+                    // look around
+                    break;
+
+                case thingsToDo.explore:
+                    // go on an adventure
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+            playerAgent.destination = goal.position;
             needTask = false;
         }
         else if(new Vector3(this.transform.position.x, 0, this.transform.position.z) == new Vector3(playerAgent.destination.x, 0, playerAgent.destination.z)) // if character has reached its destination
