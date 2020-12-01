92469e5106e3d46010d42a9582f5a80c5ddd15a6
Gavin Coffelt <gavincoffelt@yahoo.com>
2020-11-30 17:50:42 -0800

    Semi working Player controller

---
+++ Assets/Scripts/GCPlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GCPlayerController : MonoBehaviour
+{
+    #region
+    public static GCPlayerController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+    public float TurnSmoothTime = 0.2f;
+    private float turnSmoothVel;
+    private float speedSmoothVel;
+
+    public float SpeedSmoothTime = 0.1f;
+    public float curSpeed;
+
+    public float Speed;
+    public float DashInterval = 1.0f;
+    public float CurInterval;
+    public float DashSpeed = 30.0f;
+
+    private Vector3 pos;
+    private float ySpeed = 0;
+    private float gravity = 9.8f;
+
+    private Animator animator;
+    private CharacterController cc;
+    private float animspeed = 0;
+    void Start()
+    {
+        TryGetComponent(out animator);
+        TryGetComponent(out cc);
+        CurInterval = DashInterval;
+
+    }
+
+    void Update()
+    {
+        if (CurInterval > 0) { CurInterval -= Time.deltaTime; } // Dash Interval Time.
+
+        // Calculating The input of the player.
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = playerInput.normalized;
+
+        // if the input Direction isnt zero then calculate the target angle and smooth the angle between the two.
+        if (inputDir != Vector2.zero)
+        {
+            float targetAngle = (Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg) + Camera.main.transform.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, TurnSmoothTime);
+        }
+
+        float targetSpeed = (Speed) * inputDir.magnitude; // Targetspeed calculation.
+
+        // If Space key is pressed then initiate Dash and Dash animation.
+        if (Input.GetKeyDown(KeyCode.Space) && CurInterval <= 0)
+        {
+            animator.SetTrigger("Dash"); //trigger Dash
+            curSpeed = curSpeed * DashSpeed;
+            CurInterval = DashInterval;
+
+        }
+        else { curSpeed = Mathf.SmoothDamp(curSpeed, targetSpeed, ref speedSmoothVel, SpeedSmoothTime); }
+        if (cc.isGrounded) { ySpeed = 0; } // If it is grounded, no gravity;
+
+        // If the character is Animating the idle-run OR is dashing.
+          ySpeed -= gravity * Time.deltaTime;
+
+            pos.x = transform.forward.x;
+            pos.y = ySpeed;
+            pos.z = transform.forward.z;
+            cc.Move(pos * curSpeed * Time.deltaTime);
+
+        if(curSpeed > 4 && animspeed < 1) { animspeed += .01f; }
+        else if (animspeed > 0){ animspeed -= .01f; }
+        animator.SetFloat("speed", animspeed);
+        Debug.Log(animspeed);
+    }
+
+}
---
+++ Assets/Scripts/New3rdPersonCam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New3rdPersonCam : MonoBehaviour
+{
+    [Header("CameraAttributes")]
+    public Transform Target;
+    public float TargetDist;
+    public float MouseSensitivity;
+
+    private float yAxis;
+    private float xAxis;
+    private Vector2 vertMax = new Vector2(-20, 65);
+
+    public float rotSmoothTime = .12f;
+    private Vector3 rotSmoothVel;
+    Vector3 currentRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        xAxis += Input.GetAxis("Mouse X") * MouseSensitivity;
+        yAxis -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+        yAxis = Mathf.Clamp(yAxis, vertMax.x, vertMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(yAxis, xAxis), ref rotSmoothVel, rotSmoothTime);
+
+        transform.eulerAngles = currentRotation;
+
+        transform.position = Target.position - transform.forward * TargetDist;
+        if (Time.timeScale == 0)
+        {
+            Cursor.visible = true;
+        }
+    } 
+}
